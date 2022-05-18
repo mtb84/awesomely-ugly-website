1884d9a3e530aea25016ad19c4df67f0c9197cc0
MATTHEW TYLER BARTON <mtb@MATTHEWs-MacBook-Pro.local>
2022-05-17 22:58:05 -0400

    FINAL

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,11 @@
-// let button = document.querySelector('button');
-//   button.addEventListener('click', function(event){
-//       var image = document.getElementsByClassName('image');
-//       if (image.src.match("https://i1.sndcdn.com/avatars-000117078572-q59edo-t500x500.jpg" )) {
-//           image.src = 'https://i.pinimg.com/originals/05/1e/37/051e37662a8d8927fab62d790f3ae8bd.jpg';
-//       } else {
-//           image.src = "https://i1.sndcdn.com/avatars-000117078572-q59edo-t500x500.jpg";
-//       }
-//   });
 
+const imgButton = document.querySelector('button');
 
-  let swapButton = document.querySelector('button');
-let image = document.querySelector('img');
-
-swapButton.addEventListener('click', () => {
-    image.src = 'https://i.pinimg.com/originals/05/1e/37/051e37662a8d8927fab62d790f3ae8bd.jpg';
-})
+imgButton.addEventListener('click', function(){
+    const image = document.getElementById('swap');
+    if (image.src.match("https://i1.sndcdn.com/avatars-000117078572-q59edo-t500x500.jpg")) {
+        image.src = "https://i.pinimg.com/originals/05/1e/37/051e37662a8d8927fab62d790f3ae8bd.jpg";
+    } else {
+        image.src = "https://i1.sndcdn.com/avatars-000117078572-q59edo-t500x500.jpg";
+    }
+});
